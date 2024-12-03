d475a9782c4e6076aee8b98fcb89cdfd9c63e99c
ntalluri <nehatalluri@live.com>
2024-12-03 17:28:15 -0600

    got rid of shuffle

--- src/finetuning.py
+++ src/finetuning.py
@@ -214,9 +214,9 @@ val_dataset = HDF5IterableDataset(val_file_paths, padding_strategy = padding, po
 test_dataset = HDF5IterableDataset(test_file_paths, padding_strategy = padding, pos_encoding_method=positional_encodings)
  
 # Create data loaders
-train_loader = DataLoader(train_dataset, batch_size=batch, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=batch)
+val_loader = DataLoader(val_dataset, batch_size=batch)
+test_loader = DataLoader(test_dataset, batch_size=batch)
 
 for epoch in range(num_epochs):
     print(f"Epoch {epoch + 1}")
